9891f5ba7e879d9fcd15d3e5d81a39270b6ba386
YADAVMANISH97 <yadavmanish8844@gmail.com>
2026-02-15 01:51:56 +0530

    add comment in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Notes API');
+    res.send('Hello World! This is the Note Taking App API.');
 });
 
 // Mount routes
